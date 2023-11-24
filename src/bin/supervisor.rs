3e2fbd189f9fbe131c4839c7c900545876c34427
Solomon Jacobs <solojacobs@gmail.com>
2023-11-24 19:39:38 +0100

    Allow specifying working_directory

--- src/bin/supervisor.rs
+++ src/bin/supervisor.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use scheduler::sys;
+use std::fs::{self, File};
+use std::path::{Path, PathBuf};
 use std::io::{self, Write};
 use tokio::process::Command;
 use tokio::signal::ctrl_c;
@@ -10,6 +12,8 @@ use tokio_util::sync::CancellationToken;
 struct Arguments {
     #[arg(short, long)]
     command: String,
+    #[arg(short, long)]
+    working_directory: PathBuf,
     #[arg(short, long, default_value = "60")]
     interval: u64,
     #[clap(last = true)]
@@ -18,8 +22,6 @@ struct Arguments {
 
 #[tokio::main]
 async fn main() {
-    let pid = std::process::id();
-    println!("{pid}");
     let arguments = Arguments::parse();
 
     let mut command = Command::new(arguments.command);
@@ -28,27 +30,40 @@ async fn main() {
     let interval = time::interval(time::Duration::from_secs(arguments.interval));
 
     let token = CancellationToken::new();
-    let k = tokio::spawn(run(command, interval, token.clone()));
+    let runner = tokio::spawn(run(
+        command,
+        interval,
+        token.clone(),
+        arguments.working_directory.clone(),
+    ));
     let _ = ctrl_c().await;
     token.cancel();
-    let _ = k.await;
+    let _ = runner.await;
 }
 
-async fn run(mut command: Command, mut interval: time::Interval, token: CancellationToken) {
+async fn run(
+    mut command: Command,
+    mut interval: time::Interval,
+    token: CancellationToken,
+    working_directory: std::path::PathBuf,
+) {
+    let mut i = 0;
     loop {
         tokio::select! {
-            _ = interval.tick() => {}
+            _instant = interval.tick() => { }
             _ = token.cancelled() => { return }
         }
         println!("Starting");
+
+        let run_directory = working_directory.join(format!("{i}"));
+        command = setup(command, &run_directory);
+        let mut status = create_file(&run_directory.join("status")).unwrap();
         let child = command.spawn().expect("Failed to execute command");
         if let Some(child_id) = child.id() {
             tokio::select! {
                 output = child.wait_with_output() => {
                     let outcome = output.expect("Failed to execute command");
-                    io::stdout().write_all(&outcome.stdout).unwrap();
-                    io::stderr().write_all(&outcome.stderr).unwrap();
-                    println!("{:?}", outcome.status.code());
+                    status.write_all(format!("{:?}", outcome.status).as_bytes()).unwrap();
                 }
                 _ = token.cancelled() => {
                     println!("KILLING");
@@ -61,5 +76,29 @@ async fn run(mut command: Command, mut interval: time::Interval, token: Cancella
                 }
             }
         }
+        i += 1;
     }
+}
+
+#[derive(Debug)]
+struct WithPath{
+    error: io::Error,
+    path: PathBuf,
+}
+
+fn create_file(path: &Path) -> Result<File, WithPath> {
+    File::create(path).map_err(|error| WithPath{error, path: path.into()})
+}
+
+fn create_dir(path: &Path) -> Result<(), WithPath> {
+    fs::create_dir(path).map_err(|error| WithPath{error, path: path.into()})
+}
+
+fn setup(mut command: Command, run_directory: &Path) -> Command {
+    create_dir(&run_directory).unwrap();
+    let stdout = create_file(&run_directory.join("stdout")).unwrap();
+    let stderr = create_file(&run_directory.join("stderr")).unwrap();
+    command.stdout(stdout);
+    command.stderr(stderr);
+    command
 }
