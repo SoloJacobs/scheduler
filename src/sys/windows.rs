a7f85ebe2f1bf7fb895df68a421ea6630c5e120d
Solomon Jacobs <solojacobs@gmail.com>
2023-11-28 18:47:13 +0100

    rename to interrupt

--- src/sys/windows.rs
+++ src/sys/windows.rs
@@ -3,7 +3,7 @@ use windows::Win32::System::Console::GenerateConsoleCtrlEvent;
 use windows::Win32::System::Console::SetConsoleCtrlHandler;
 use windows::Win32::System::Console::CTRL_C_EVENT;
 
-pub fn kill_gracefully() {
+pub fn interrupt() {
     {
         unsafe { SetConsoleCtrlHandler(None, true).expect("ERROR") };
         unsafe { GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0).expect("ERROR") };
